42a1ecddde4fe3cf7d839625bfb00f2de5eb9f09
thahmina-ali <thahmina.ali@biscglobal.com>
2022-02-25 17:23:05 +0000

    docker fix snaq-seq.sh

--- snaq-seq.sh
+++ snaq-seq.sh
@@ -99,10 +99,11 @@ norm_amp=$(echo ${5} |sed 's/.*=//g')
 genome_fasta=$(echo ${ref} | sed 's@.*/@@')
 genome_path=$(echo ${ref} | sed 's/\/[^/]*$/\//')
 
+docker pull accugenomics/snaq-seq:v1
 
 # If options verified from user.
-docker run -e genome_fasta="$genome_fasta" -e outsam="$6" -e ofs="$7" -e mfs="$8" -e rc="$9" -e mpq="${10}" -e qc="${11}"  -e gbc="${12}" -e outis=${13}   -e cc="${14}" -e is=${15}  -v $input:/home/data/input  -v $output:/home/data/output  -v $genome_path:/home/data/ref  -v $bc_amp:/home/data/basechange/amplicon_basechange.txt -v $norm_amp:/home/data/normalization/amplicon_normalization.txt -ti accugenomics/snaq-seq:v1 
+docker run -e genome_fasta="$genome_fasta" -e outsam="$6" -e ofs="$7" -e mfs="$8" -e rc="$9" -e mpq="${10}" -e qc="${11}"  -e gbc="${12}" -e outis=${13}   -e cc="${14}" -e is=${15}  -v $input:/home/data/input  -v $output:/home/output -v $genome_path:/home/data/ref  -v $bc_amp:/home/data/basechange/amplicon_basechange.txt -v $norm_amp:/home/data/normalization/amplicon_normalization.txt -ti accugenomics/snaq-seq:v1 bash /snaq-seq/init.sh
 
-
+exit
 
 
