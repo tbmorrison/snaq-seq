4409b422216a52ee362bae311cf284760238592b
thahmina-ali <thahmina.ali@biscglobal.com>
2022-03-25 04:08:50 +0000

    make_dir.sh comment

--- snaq-vsoft_container-v1/make_dir.sh
+++ snaq-vsoft_container-v1/make_dir.sh
@@ -1,4 +1,7 @@
 # author: "Thahmina A. Ali"
+# Refer to the README and flowchart
+# Creates working directory to perform the analysis.
+
 
 ls /home/data/input/*_R1_* | awk 'BEGIN {FS="_R1|/"};  {print "mkdir /home/output/TIME/"$5"  &"}' > make_dir_list.sh
 
