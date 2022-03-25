4edba613861713afc435f9f51b213fc510aa9407
davidlorenz <davidlorenz@users.noreply.github.com>
2022-03-25 00:07:12 -0400

    Update make_dir.sh

--- snaq-vsoft_container-v1/make_dir.sh
+++ snaq-vsoft_container-v1/make_dir.sh
@@ -1,4 +1,9 @@
+# make_dir.sh
+#
 # author: "Thahmina A. Ali"
+#
+# Usage: accessory script to create/initate alignment launch_list files; please refer to README.md for program flow and further information.
+
 
 ls /home/data/input/*_R1_* | awk 'BEGIN {FS="_R1|/"};  {print "mkdir /home/output/TIME/"$5"  &"}' > make_dir_list.sh
 
