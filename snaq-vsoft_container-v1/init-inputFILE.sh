a3d2af99edbae359f19786a75b8149526b1c63f0
davidlorenz <davidlorenz@users.noreply.github.com>
2022-04-20 15:12:28 -0400

    Update init-inputFILE.sh

--- snaq-vsoft_container-v1/init-inputFILE.sh
+++ snaq-vsoft_container-v1/init-inputFILE.sh
@@ -15,18 +15,18 @@ export PATH=$PATH:/home/
 
 
 if [ $outsam = "outputSAM=0" ]; then
-	echo " ls /home/data/input/FILE_R1_* |  awk 'BEGIN {FS=\"_R1|/\"};  {print \"\"\$5\"\t$option0 $option1 $option2 $option3 $option4 $option5 $option6 $option7 $option8 $option9 $option10 $option11 $option12 $option13 $option14 $option15\"}' > /home/output/TIME/commandLine.txt" > /home/output_1.sh
+	echo -e "echo \"$inputFILE_fasta\t$option0 $option1 $option2 $option3 $option4 $option5 $option6 $option7 $option8 $option9 $option10 $option11 $option12 $option13 $option14 $option15\" > /home/output/TIME/commandLine.txt" > /home/output_1.sh
 	date=$(stat /home/data/input/ | grep Modify | sed 's/\s/\t/g' | cut -f2,3 | sed 's/[.].*$//' | sed 's/\t/ /g')
-	echo " ls /home/data/input/FILE_R1_* |  awk 'BEGIN {FS=\"_R1|/\"};  {print \"\"\$5\"\t$date\t\"\$1\"/\"\$2\"/\"\$3\"/\"\$4\"/\"\$5\"_R1\"\$6\";\"\$1\"/\"\$2\"/\"\$3\"/\"\$4\"/\"\$5\"_R2\"\$6\"\"}' > /home/output/TIME/fastqCreationDate.txt" > /home/output_2.sh 
-	       perl /snaq-seq/snaq-seq-if_core.pl $inputFILE_fasta $genome_fasta $outsam $ofs $mfs $rc $mpq $qc $gbc $outis $cc $is
+	echo -e "echo \"$inputFILE_fasta\t$date\t$inputFILE_path$inputFILE_R1;$inputFILE_path$inputFILE_R2\" > /home/output/TIME/fastqCreationDate.txt" > /home/output_2.sh
+	perl /snaq-seq/snaq-seq-if_core.pl $inputFILE_R1 $inputFILE_R2  $inputFILE_fasta $genome_fasta $outsam $ofs $mfs $rc $mpq $qc $gbc $outis $cc $is
 fi
 
 
 if [ $outsam = "outputSAM=1" ]; then
-	echo " ls /home/data/input/FILE_R1_* |  awk 'BEGIN {FS=\"_R1|/\"};  {print \"\"\$5\"\t$option0 $option1 $option2 $option3 $option4 $option5 $option6 $option7 $option8 $option9 $option10 $option11 $option12 $option13 $option14 $option15\"}' > /home/output/TIME/commandLine.txt" > /home/output_1.sh
+	echo -e "echo \"$inputFILE_fasta\t$option0 $option1 $option2 $option3 $option4 $option5 $option6 $option7 $option8 $option9 $option10 $option11 $option12 $option13 $option14 $option15\" > /home/output/TIME/commandLine.txt" > /home/output_1.sh
 	date=$(stat /home/data/input/ | grep Modify | sed 's/\s/\t/g' | cut -f2,3 | sed 's/[.].*$//' | sed 's/\t/ /g')	
-	echo " ls /home/data/input/FILE_R1_* |  awk 'BEGIN {FS=\"_R1|/\"};  {print \"\"\$5\"\t$date\t\"\$1\"/\"\$2\"/\"\$3\"/\"\$4\"/\"\$5\"_R1\"\$6\";\"\$1\"/\"\$2\"/\"\$3\"/\"\$4\"/\"\$5\"_R2\"\$6\"\"}' > /home/output/TIME/fastqCreationDate.txt" > /home/output_2.sh
-	perl /snaq-seq/snaq-seq-if_core-sam.pl $inputFILE_fasta $genome_fasta $outsam $ofs $mfs $rc $mpq $qc $gbc $outis $cc $is
+	echo -e "echo \"$inputFILE_fasta\t$date\t$inputFILE_path$inputFILE_R1;$inputFILE_path$inputFILE_R2\" > /home/output/TIME/fastqCreationDate.txt" > /home/output_2.sh
+	perl /snaq-seq/snaq-seq-if_core-sam.pl $inputFILE_R1 $inputFILE_R2 $inputFILE_fasta $genome_fasta $outsam $ofs $mfs $rc $mpq $qc $gbc $outis $cc $is
 fi
 
 
