536bd59b3fcc72d11aff5c5940f8d629d33c9490
davidlorenz <davidlorenz@users.noreply.github.com>
2022-04-20 15:24:58 -0400

    Update snaq-seq_core.pl

--- snaq-vsoft_container-v1/snaq-seq_core.pl
+++ snaq-vsoft_container-v1/snaq-seq_core.pl
@@ -22,8 +22,6 @@
 #
 ## Example: perl /snaq-seq/snaq-seq_core.pl /home/data/input/ hg19 0 0.01 0 1 1 0  1 0 300 300 
 
-
-
 use strict;
 use warnings;
 use Time::Piece;
@@ -34,8 +32,8 @@ my $time = localtime->strftime('%Y-%m-%d_%H:%M:%S');
 my $inputDIR= $ARGV[0];
 my $genome_fasta= $ARGV[1];
 my $outputSAM= $ARGV[2];
-my $ofsCutoff= $ARGV[3];
-my $msf= $ARGV[4];
+my $offspringCutoff= $ARGV[3];
+my $mfs= $ARGV[4];
 my $RC= $ARGV[5];
 my $mapq= $ARGV[6];
 my $qCutoff= $ARGV[7];
@@ -46,7 +44,7 @@ my $IS= $ARGV[11];
 
 
 
-my $launch_1 = "mkdir /home/output/$time; cd /home/ ; bash /snaq-seq/make_dir.sh; bash /snaq-seq/snaq-seq_command.sh; bash /snaq-seq/snaq-seq_launch.sh; cp /snaq-seq/pool_lanes.sh /home/output/$time; cp /snaq-seq/no_lanes.sh /home/output/$time; cp /snaq-seq/count_files.sh /home/output/$time; sed -i \'s/TIME/$time/g\' /home/output/$time/count_files.sh; sed -i \'s/TIME/$time/g\' /home/output/$time/pool_lanes.sh; sed -i \'s/TIME/$time/g\' /home/output/$time/no_lanes.sh;  sed -i \'s/TIME/$time/g\'  make_dir_list.sh; sed -i \'s/TIME/$time/g\'  snaq-seq_launch_list.sh; sed -i \'s/TIME/$time/g\' snaq-seq_runs_list.sh;  sed -i \'s/GENOME/$genome_fasta/g\' snaq-seq_runs_list.sh;  sed -i \'s/REPLACE/-v $ofsCutoff -v $msf -v $RC -v $mapq -v $qCutoff -v $gbc -v $outputIS/g\' snaq-seq_runs_list.sh; sed -i \'s/TIME/$time/g\' output_1.sh; sed -i \'s/TIME/$time/g\' output_2.sh; bash snaq-seq_runs_list.sh"; 
+my $launch_1 = "mkdir /home/output/$time; cd /home/ ; bash /snaq-seq/make_dir.sh; bash /snaq-seq/snaq-seq_command.sh; bash /snaq-seq/snaq-seq_launch.sh; sed -i \'s/TIME/$time/g\'  make_dir_list.sh; sed -i \'s/TIME/$time/g\'  snaq-seq_launch_list.sh; sed -i \'s/TIME/$time/g\' snaq-seq_runs_list.sh;  sed -i \'s/GENOME/$genome_fasta/g\' snaq-seq_runs_list.sh;  sed -i \'s/REPLACE/-v $offspringCutoff -v $mfs -v $RC -v $mapq -v $qCutoff -v $gbc -v $outputIS/g\' snaq-seq_runs_list.sh; sed -i \'s/TIME/$time/g\' output_1.sh; sed -i \'s/TIME/$time/g\' output_2.sh; bash snaq-seq_runs_list.sh"; 
 system($launch_1);
 wait;
 print "******************************************************\n";
@@ -74,15 +72,15 @@ print "\n";
 wait;
 
 
-my $launch_4 = "cd /home/output/$time/; cat /home/output/$time/*/PassCount.txt > /home/output/$time/PassCount.txt; cat /home/output/$time/*/BadCount.txt > /home/output/$time/BadCount.txt; cat /home/output/$time/*/BaseCount.txt > /home/output/$time/BaseCount.txt;  cat /home/output/$time/*/mapqCount.txt > /home/output/$time/mapqCount.txt; cat /home/output/$time/*/NMCount.txt > /home/output/$time/NMCount.txt; cat /home/output/$time/*/offTargetCount.txt > /home/output/$time/offTargetCount.txt; cat /home/output/$time/*/unmappedCount.txt > /home/output/$time/unmappedCount.txt;  cat /home/output/$time/*/ComplexityCount.txt > /home/output/$time/ComplexityCount.txt; bash /home/output_1.sh;  bash /home/output_2.sh";
+my $launch_4 = "cd /home/output/$time/; cat -- '/home/output/$time/'*'/-PassCount.txt' > /home/output/$time/PassCount.txt; cat -- '/home/output/$time/'*'/-BadCount.txt' > /home/output/$time/BadCount.txt; cat -- '/home/output/$time/'*'/-BaseCount.txt' > /home/output/$time/BaseCount.txt;  cat -- '/home/output/$time/'*'/-mapqCount.txt' > /home/output/$time/mapqCount.txt; cat -- '/home/output/$time/'*'/-NMCount.txt' > /home/output/$time/NMCount.txt; cat -- '/home/output/$time/'*'/-offTargetCount.txt' > /home/output/$time/offTargetCount.txt; cat -- '/home/output/$time/'*'/-unmappedCount.txt' > /home/output/$time/unmappedCount.txt;  cat -- '/home/output/$time/'*'/-ComplexityCount.txt' > /home/output/$time/ComplexityCount.txt; bash /home/output_1.sh;  bash /home/output_2.sh";
 system($launch_4);
 print "\n";
 print "Snaq-seq analysis finalizing...\n";
 print "\n";
 wait;
 
-my $launch_5 = "Rscript /snaq-seq/CombineThreads.R '/home/output/$time' '/home/data/output' '/home/data/normalization' >> /home/error2" ; 
-#cp /home/output/$time/SNAQ-SEQ*  /home/data/output/$time/";
+
+my $launch_5 = "Rscript /snaq-seq/CombineThreads.R '/home/output/$time' '/home/data/output' '/home/data/normalization' $offspringCutoff >> /home/error2" ; 
 system($launch_5);
 
 
