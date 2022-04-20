747c30f0a603eff3dbaa3de1ec4f3764b6880f9a
davidlorenz <davidlorenz@users.noreply.github.com>
2022-04-20 15:15:40 -0400

    Update snaq-seq-if_command-sam.sh

--- snaq-vsoft_container-v1/snaq-seq-if_command-sam.sh
+++ snaq-vsoft_container-v1/snaq-seq-if_command-sam.sh
@@ -2,6 +2,29 @@
 # Usage: Simulates the commands for the core analysis that multithreads the run process (bwa) and memory for the counting algorithm (awk) amd generation of filtered reads and aligned reads (sam) for a single sample. 
 # Please refer to README.md for program flow and further information.
 
-ls /home/data/input/FILE_R1_* | awk 'BEGIN {FS="_R1|/"};  {print "echo \"cd /home/output/TIME/FILE/ ;  echo Analysis FILE...run start time: \\$(date +%T); bwa mem -v 1 -R \\\"@RG\\\\tID:FILE\\\\tSM:FILE\\\\tLB:NA\\\\tPL:SNAQ\\\" -t 8 /home/data/ref/GENOME "$1"/"$2"/"$3"/"$4"/FILE_R1"$6" "$1"/"$2"/"$3"/"$4"/FILE_R2"$6" | parallel -k -q --block 10M --pipe  awk -f /snaq-seq/remRecombo.awk -v sampleName=FILE -v outputSAM=/home/data/output REPLACE /home/data/basechange/amplicon_basechange.txt - | parallel -k -q --block 10M --pipe awk -f /snaq-seq/makeFastq.awk -v file1=/home/data/output/FILE_R1_001.fastq -v file2=/home/data/output/FILE_R2_001.fastq - ; echo Analysis FILE...run end time: \\$(date +%T) \"  > /home/FILE_snaq-seq.sh  "}' > snaq-seq_runs_list.sh
+ref=/home/data/ref/GENOME 
+t="8"
+v="1"
+cores=$(lscpu | grep "CPU(s):" | head -1 | sed 's/\s//g' | cut -d":" -f2)
+R1="$1"
+R2="$2"
+
+export ref
+export t
+export v
+export cores
+export R1
+export R2
+
+snaq_vsoft() {
 
 
+bwa mem -v $v -R "@RG\tID:FILE\tSM:FILE\tLB:NA\tPL:SNAQ" -t $t /home/data/ref/GENOME /home/data/input/inputFILE_R1 /home/data/input/inputFILE_R2 | parallel --pipe -k  awk -f /snaq-seq/remRecombo.awk -v sampleName=FILE -v samOutput=1 REPLACE /home/data/basechange/amplicon_basechange.txt - |  awk -f /snaq-seq/makeFastq.awk -v file1=/home/output/TIME/FILE/FILE_R1_001.fastq -v file2=/home/output/TIME/FILE/FILE_R2_001.fastq -  ; bgzip --threads $cores < /home/output/TIME/FILE/FILE_R1_001.fastq > /home/output/TIME/FILE/FILE_R1_001.fastq.gz; bgzip --threads $cores < /home/output/TIME/FILE/FILE_R2_001.fastq > /home/output/TIME/FILE/FILE_R2_001.fastq.gz;  mv /home/output/TIME/FILE/FILE_R*_001.fastq.gz  /home/data/output/
+
+}
+
+export -f snaq_vsoft
+
+parallel -j $t --verbose --joblog jobRuntime.log -k snaq_vsoft ::: /home/data/input/inputFILE_R1 ::: /home/data/input/inputFILE_R2 ; 
+
+cut -f4 jobRuntime.log | tail -1 | awk '{print "Core analysis (alignment, counting etc.)...job run time: " $1}'
